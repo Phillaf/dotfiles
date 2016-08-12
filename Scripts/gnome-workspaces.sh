38396e4cc822ab6d1454f4d4013ec6e59586d407
Philippe Lafrance <phil.laf@gmail.com>
2016-08-12 16:05:53 -0400

    sample of the hotkeys

--- Scripts/gnome-workspaces.sh
+++ Scripts/gnome-workspaces.sh
@@ -1,2 +1,5 @@
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+
+dconf write /org/gnome/desktop/wm/keybindings switch-windows '<Alt>Tab'
+dconf write /org/gnome/desktop/wm/keybindings switch-applications '<Super>Tab'
