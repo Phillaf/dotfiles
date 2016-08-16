d5a933cbc4878fd28c345e012f6f9f8c8966d870
Philippe Lafrance <phil.laf@gmail.com>
2016-08-16 00:48:09 -0400

    docker script split in permissions/install

---
+++ Scripts/docker-permissions.sh
@@ -0,0 +1,7 @@
+sudo groupadd docker
+
+sudo gpasswd -a ${USER} docker
+
+sudo systemctl restart docker
+
+newgrp docker
--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -1,11 +1,3 @@
 sudo dnf install docker
 
 sudo systemctl start docker
-
-sudo groupadd docker
-
-sudo gpasswd -a ${USER} docker
-
-sudo serviec docker restart
-
-newgrp docker
