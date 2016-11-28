f62fd620bbee2bd9f873610937ff2e1c379c608b
Philippe Lafrance <philippe.lafrance@mindgeek.com>
2016-11-28 12:03:52 -0500

    Also install docker-compose

--- .scripts/docker.sh
+++ .scripts/docker.sh
@@ -1,3 +1,7 @@
 sudo dnf install docker
 
 sudo systemctl start docker
+
+# See the latest release version https://github.com/docker/compose/releases
+sudo curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
