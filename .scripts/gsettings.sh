66929c7226dbb1f4ba612b8366ddd0b62f26be6a
Philippe Lafrance <phil.laf@gmail.com>
2016-11-30 12:06:14 -0500

    Alt+tab switched between windows, not apps

--- .scripts/gsettings.sh
+++ .scripts/gsettings.sh
@@ -4,14 +4,21 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-side-e "['<Shift><Alt>l']
 gsettings set org.gnome.desktop.wm.keybindings move-to-side-w "['<Shift><Alt>h']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Shift><Alt>j', '<Control><Shift><Alt>Down']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Shift><Alt>k', '<Control><Shift><Alt>Up']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Primary><Alt>j', '<Control><Alt>Up']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Primary><Alt>j', '<Control><Alt>Up']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Primary><Alt>k', '<Control><Alt>Up']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings unmaximize "['<Super>j', '<Alt>F5']"
+
 gsettings set org.gnome.mutter.keybindings toggle-tiled-left "['<Super>h']"
 gsettings set org.gnome.mutter.keybindings toggle-tiled-right "['<Super>l']"
+
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+
 gsettings set org.gnome.settings-daemon.plugins.media-keys www '<Super>w'
 gsettings set org.gnome.settings-daemon.plugins.media-keys home '<Super>e'
 
