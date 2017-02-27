90be8ce8b185a35ce2befe06578fed8d06a5f643
Philippe Lafrance <philippe.lafrance@mindgeek.com>
2017-02-27 16:07:29 -0500

    msgpack installed with pecl

--- .scripts/install/nvim.sh
+++ .scripts/install/nvim.sh
@@ -2,13 +2,9 @@
 sudo dnf -y install neovim
 sudo dnf -y install python2-neovim python3-neovim
 
-git clone https://github.com/msgpack/msgpack-php.git
-cd msgpack-php
-phpize
-./configure
-make && sudo make install
-cd ../
-rm -rf msgpack-php/
+pecl install msgpack
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+sudo dnf install xclip
