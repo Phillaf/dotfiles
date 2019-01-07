d255ec09827db6a061688f4f0772e3c7c05104e2
Philippe Lafrance <phil.laf@gmail.com>
2019-01-07 14:20:55 -0500

    Add docker-compose and remove-apache scripts

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -4,7 +4,7 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce docker-compose
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
---
+++ Scripts/remove-apache.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo service apache2 stop
+sudo apt-get purge apache2
