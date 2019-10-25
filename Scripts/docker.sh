e3f0549be8d44ad0bca52c1d1b071680804054db
Mathieu Aubin <mathieu@zeroserieux.com>
2019-10-25 18:38:46 -0400

    version update
    pourquoi pas, tant qu'a passé dans le coin!

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -10,7 +10,7 @@ sudo dnf install -y docker-ce
 
 sudo systemctl start docker
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo groupadd docker
